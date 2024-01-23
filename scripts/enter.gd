d4c88be22cdf0caabb1e47498dfa619e7c93c517
Kono <75730503+Kono-o@users.noreply.github.com>
2024-01-23 20:13:23 +0530

    file io works lfgggg

--- scripts/enter.gd
+++ scripts/enter.gd
@@ -10,21 +10,13 @@ signal update_name(newName:String)
 func keywordEngine():
 	TerminalLine.text = ""
 	keywordArray = command.split(' ',false, 64)
-	#for i in keywordArray.size():
-	#	print(keywordArray[i])
 	
 	if keywordArray.size() > 2:
 		if keywordArray[0] == "name":
 			if keywordArray [1] == "set":
 				update_name.emit(keywordArray[2])
 
-
 func _on_button_up():
 	command = TerminalLine.text
 	keywordEngine()
 
-func _ready():
-	pass
-
-func _process(delta):
-	pass
--- scripts/username.gd
+++ scripts/username.gd
@@ -1,20 +1,36 @@
 extends Label
 
+var username
+const DEF_NAME = "user"
+var filePath = "user://name.dat"
+
 @onready var UsernameLabel = get_node("/root/App/UI/UsernameLabel")
 
-@export var data: Resource
+func writeName(n):
+	var writeNameFile = FileAccess.open(filePath,FileAccess.WRITE)
+	writeNameFile.store_string(n)
+	print_debug(n + " written")
+
+func readName():
+	if FileAccess.file_exists(filePath):
+		var readNameFile = FileAccess.open(filePath,FileAccess.READ)
+		username = readNameFile.get_as_text()
+		if username == "":
+			username = DEF_NAME
+			writeName(DEF_NAME)
+	else:
+		print_debug("username file not found")
+		writeName(DEF_NAME)
+	print_debug(str(username) + " read")
 
 func _ready():
-	data.loadData()
-	usernameSet(data.name)
+	readName()
+	usernameSet(username)
 
-func usernameSet(local_name):
-	data.name = local_name
-	UsernameLabel.text = data.name
+func usernameSet(n):
+	writeName(n)
+	UsernameLabel.text = n
 
 func _on_enter_button_update_name(newName):
 	usernameSet(newName)
-	data.saveData()
 	
-func _process(delta):
-	pass
