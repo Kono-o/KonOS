194ff25eaa2a9cff1ccf262c7dfce79dd3f43f60
Kono <75730503+Kono-o@users.noreply.github.com>
2024-01-31 19:15:56 +0530

    color lerping

--- scripts/chart-box.gd
+++ scripts/chart-box.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var chart_label_1 = get_node("/root/App/UI/chart-box/chart-label-1")
 
-const COLOR_ARRAY = ["#ffffff","#4a4a4a"]
+#const COLOR_ARRAY = ["#fcba03","#0373fc"]
+const COLOR_ARRAY = ["#ffffff","#000000"]
 
 var color_array = []
 
@@ -10,22 +11,45 @@ func strToCol():
 	color_array.resize(COLOR_ARRAY.size())
 	for i in COLOR_ARRAY.size():
 		color_array[i] = Color(COLOR_ARRAY[i]).to_html()
-func col(c):
-	return "[color=%s]" % [color_array[c]]
+func col(l):
+	var col1 = Color(COLOR_ARRAY[0])
+	var col2 = Color(COLOR_ARRAY[1])
+	return "[color=%s]" % [col1.lerp(col2,l).to_html()]
 
-func _ready():
-	strToCol()
+func textSet(arr):
 	var text = ""
-	var tog = 0
-	for i in 182:
-		if i % 2 == 0:
-			text = '%s%s.' % [text,col(tog)]
-		if i % 2 == 1:
-			text = '%s%s.' % [text,col(abs(1-tog))]
-		if i % 26 == 25 and i != 0:
-			text = '%s\n' % [text]
-			tog = abs(1-tog)
+	var array = []
+	array.resize(arr.size())
 	
-	chart_label_1.text = text
-func _process(delta):
+	var max = 0.0
+	var min = 0.0
+	for i in arr.size():
+		if arr[i] > max:
+			max = arr[i]
+		if arr[i] < min:
+			min = arr[i]
+	
+	for i in array.size():
+		array[i] = (arr[i]-min)/(max-min)
+	
+	for i in 7:
+		for j in 26:
+				text = '%s%s.' % [text,col(array[(j*7)+i])]
+		text = '%s%s' % [text,"\n"]
+	
+	return text
+	
+func _ready():
 	pass
+	
+func _process(delta):
+	var rng = RandomNumberGenerator.new()
+	rng.seed = int(Time.get_unix_time_from_system())
+	strToCol()
+	
+	var arry = []
+	arry.resize(182)
+	for i in 182:
+		arry[i] = rng.randf_range(0, 181)
+	
+	chart_label_1.text = textSet(arry)
